ae4d8ea4231663115e1b7e914478bb50be26c3cc
SpaceDev - Yash Wamane <spacepointtech@gmail.com>
2024-12-05 16:22:05 +0530

    Update Join Now!

--- components/ThirdSection.js
+++ components/ThirdSection.js
@@ -22,8 +22,8 @@ export default function Third() {
         
       <div className="w-[1093px] h-[808.24px] left-[203px] top-[213.69px] absolute">
         <div className="w-[449px] h-[62px] left-[345px] top-[544.97px] absolute justify-start items-start gap-[41.20px] inline-flex">
-          <div className="grow shrink basis-0 h-[62px] px-[74px] py-4 rounded-lg border border-[#e97f18] justify-center items-center gap-2.5 flex">
-            <div className="w-[94px] text-[#e97f18] text-xl font-semibold hover"> <Link href="/signup">Join Now</Link> </div>
+          <div className="w-[94px] text-[#e97f18] text-xl font-semibold grow shrink basis-0 h-[62px] px-[74px] py-4 rounded-lg border border-[#e97f18] justify-center items-center gap-2.5 flex hover:text-white hover:bg-[#e97f18]">
+          <Link href="/signup">Join Now</Link> 
           </div>
         </div>
 
@@ -55,66 +55,3 @@ export default function Third() {
 
   );
 }
-
-// 'use client';
-// import Image from "next/image";
-// import { FaFacebookF, FaInstagram, FaTwitter, FaWhatsapp } from "react-icons/fa";
-
-// export default function Third() {
-//   return (
-
-//     <div className="h-[1022px] relative bg-gradient-to-b from-[#141416] via-[#261300] to-[#301800]">
-//     <div className="text-center p-20">
-//          <span className="text-white text-[40px] font-bold">Why </span><span className="text-[#e97f18] text-[40px] font-bold">Artists</span><span className="text-white text-[40px] font-bold"> </span><span className="text-[#e97f18] text-[40px] font-bold">Trust</span><span className="text-white text-[40px] font-bold"> Young CEO Entertainment</span>
-//          <div className="text-center text-white text-2xl font-normal">More than distribution—your partner in success.</div>  
-//          </div>  
-        
-         
-//     <div className="w-[1093px] h-[808.24px] left-[203px] top-[213.69px] absolute">
-//         <div className="w-[449px] h-[62px] left-[345px] top-[544.97px] absolute justify-start items-start gap-[41.20px] inline-flex">
-//             <div className="grow shrink basis-0 h-[62px] px-[74px] py-4 rounded-lg border border-[#e97f18] justify-center items-center gap-2.5 flex">
-//                 <div className="w-[94px] text-[#e97f18] text-xl font-semibold font-['Poppins']">Join Now</div>
-//             </div>
-//         </div>
-//         <div className="w-[329.76px] h-[808.24px] left-[182px] top-0 absolute">
-//         </div>
-//         <div className="w-[268px] h-[52px] left-[2px] top-[32.28px] absolute">
-//             <div className="w-[210px] h-[52px] left-0 top-0 absolute rounded-lg border border-[#ebebeb]" />
-//             <div className="left-[38px] top-[11px] absolute text-center text-white text-xl font-normal font-['Poppins']">Global Reach</div>
-//         </div>
-//         <div className="w-[268px] h-[52px] left-0 top-[104.28px] absolute">
-//             <div className="w-[210px] h-[52px] left-0 top-0 absolute rounded-lg border border-[#ebebeb]" />
-//             <div className="left-[38px] top-[11px] absolute text-center text-white text-xl font-normal font-['Poppins']">Simple & Fast</div>
-//         </div>
-//         <div className="w-[268px] h-[52px] left-0 top-[176.28px] absolute">
-//             <div className="w-[210px] h-[52px] left-0 top-0 absolute rounded-lg border border-[#ebebeb]" />
-//             <div className="left-[23px] top-[11px] absolute text-center text-white text-xl font-normal font-['Poppins']">Affordable Plans</div>
-//         </div>
-//         <div className="w-[268px] h-[52px] left-0 top-[248.28px] absolute">
-//             <div className="w-[210px] h-[52px] left-0 top-0 absolute rounded-lg border border-[#ebebeb]" />
-//             <div className="left-[14px] top-[11px] absolute text-center text-white text-xl font-normal font-['Poppins']">Powerful Analytics</div>
-//         </div>
-//         <div className="w-[268px] h-[52px] left-0 top-[320.28px] absolute">
-//             <div className="w-[210px] h-[52px] left-0 top-0 absolute rounded-lg border border-[#ebebeb]" />
-//             <div className="left-[48px] top-[11px] absolute text-center text-white text-xl font-normal font-['Poppins']">Smart Links</div>
-//         </div>
-//         <div className="w-[268px] h-[52px] left-0 top-[392.28px] absolute">
-//             <div className="w-[210px] h-[52px] left-0 top-0 absolute rounded-lg border border-[#ebebeb]" />
-//             <div className="left-[11px] top-[11px] absolute text-center text-white text-xl font-normal font-['Poppins']">Dedicated Support</div>
-//         </div>
-//         <div className="w-[268px] h-[52px] left-0 top-[464.28px] absolute">
-//             <div className="w-[210px] h-[52px] left-0 top-0 absolute rounded-lg border border-[#ebebeb]" />
-//             <div className="left-[14px] top-[11px] absolute text-center text-white text-xl font-normal font-['Poppins']">Powerful Analytics</div>
-//         </div>
-//         <div className="left-[318px] top-[114.97px] absolute text-center text-white text-xl font-normal font-['Poppins']">Upload once, distribute everywhere in minutes.</div>
-//         <div className="left-[318px] top-[186.97px] absolute text-center text-white text-xl font-normal font-['Poppins']">Tailored pricing for independent artists and labels.</div>
-//         <div className="left-[318px] top-[258.97px] absolute text-center text-white text-xl font-normal font-['Poppins']">Track performance and audience insights with ease</div>
-//         <div className="left-[318px] top-[330.97px] absolute text-center text-white text-xl font-normal font-['Poppins']">Share your music with one link across all platforms.</div>
-//         <div className="left-[318px] top-[402.97px] absolute text-center text-white text-xl font-normal font-['Poppins']">24/7 assistance to help you every step of the way.</div>
-//         <div className="left-[318px] top-[474.97px] absolute text-center text-white text-xl font-normal font-['Poppins']">Track performance and audience insights with ease</div>
-//         <div className="left-[318px] top-[43.28px] absolute text-center text-white text-xl font-normal font-['Poppins']">Get your music on 40+ platforms, including Spotify, Apple Music, and Amazon.</div>
-//     </div>
-// </div>
-
-//   );
-// }
